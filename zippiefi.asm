58fcd4de5f488840b5d6cbe9bcc71ae222b27374
DavidB420 <davidbadiei123@gmail.com>
2024-02-22 16:26:06 -0500

    moved display and string stuff to seperate file

--- zippiefi.asm
+++ zippiefi.asm
@@ -1,11 +1,14 @@
 ;ZippiEFI
+;Main
 ;Copyright (C) 2024 David Badiei
+
 format pe64 efi
 entry main
 
 section '.text' executable readable
 
 include 'uefi.inc'
+include 'displaystring.asm'
 
 main:
 ;Save EFI system table that is loaded into rdx
@@ -22,65 +25,6 @@ jmp $
 ;Begin to load the file system
 call initEfiFileSystem
 call openFile
-ret
-
-;getTextMode
-getTextMode:
-;Read mode number
-mov rdx,qword [efiSystemTable]
-mov rcx,[rdx+EFI_SYSTEM_TABLE.ConOut]
-mov rax,[rcx+SIMPLE_TEXT_OUTPUT_INTERFACE.Mode]
-mov eax,[rax+4]
-push rax
-;Get rows and columns
-mov rdx,qword [efiSystemTable]
-mov rcx,[rdx+EFI_SYSTEM_TABLE.ConOut]
-mov rax,[rcx+SIMPLE_TEXT_OUTPUT_INTERFACE.QueryMode]
-pop rdx
-lea r8,[textColumns]
-lea r9,[textRows]
-sub rsp,32
-call rax
-add rsp,32
-ret
-
-;numToString
-;IN: r8 = number
-numToString:
-push rax
-push rbx
-push rcx
-push rdx
-push rdi
-;Clear number buffer
-mov rcx,5
-mov eax,0
-mov rdi,numberBuffer
-repe stosd
-;Convert each digit to ASCII then save it into stack
-xchg rax,r8
-mov rcx,0
-loopSaveNumberStack:
-mov rdx,0
-mov rbx,10
-div rbx
-add dl,30h
-push dx
-inc rcx
-test rax,rax
-jnz loopSaveNumberStack
-;Save number into buffer
-mov rdi,numberBuffer
-loopSaveNumberBuffer:
-pop ax
-stosb
-inc rdi
-loop loopSaveNumberBuffer
-pop rdi
-pop rdx
-pop rcx
-pop rbx
-pop rax
 ret
 
 initEfiFileSystem:
@@ -192,110 +136,6 @@ mov r8d,0
 mov edx,0
 mov ecx,0
 call rax
-ret
-
-;setCursorPos
-;IN: RAX = column position, RBX = row position
-setCursorPos:
-push rax
-push rbx
-push rcx
-push rdx
-push r8
-;Save row and column into proper argument registers
-push rbx
-push rax
-mov rdx,qword [efiSystemTable]
-mov rcx,[rdx+EFI_SYSTEM_TABLE.ConOut]
-mov rax,[rcx+SIMPLE_TEXT_OUTPUT_INTERFACE.SetCursorPosition]
-pop rdx
-pop r8
-;Setup shadow space for GPRs
-sub rsp,32
-call rax
-add rsp,32
-pop r8
-pop rdx
-pop rcx
-pop rbx
-pop rax
-ret
-
-;centeredPrintString
-;IN: RSI = string pointer, RBX = Row to display string on
-centeredPrintString:
-push rax
-push rcx
-push rbx
-;Run through each line, must use 0x0000, 0x00ff as newline seperator
-loopPrintCenteredString:
-;Calculate and set starting point so string is at the center
-mov rax,[textColumns]
-shr rax,1
-call getUnicodeStringLength
-push rcx
-shr rcx,1
-sub rax,rcx
-call setCursorPos
-;Display string using the regular function
-sub rsp,8
-call printString
-add rsp,8
-pop rcx
-shl rcx,1
-add rsi,rcx
-add rsi,4
-inc rbx
-cmp word [rsi-2],0x00ff
-je loopPrintCenteredString
-pop rbx
-pop rcx
-pop rax
-ret
-
-;getUnicodeStringLength
-;IN: RSI = string pointer
-;OUT: RCX = length of string
-getUnicodeStringLength:
-push rax
-push rsi
-;Character is word lengthed, keep counting until end of string or newline
-mov rcx,0
-loopReadStringValues:
-lodsw
-test ax,ax
-jz gotStringLength
-cmp ax,0x0d
-je gotStringLength
-cmp ax,0xff
-je gotStringLength
-inc rcx
-jmp loopReadStringValues
-gotStringLength:
-pop rsi
-pop rax
-ret
-
-;printString
-;IN: RSI = string pointer
-printString:
-push rdx
-push rcx
-push rax
-push rsi
-;Get ConOut in rcx, then use that to get the pointer for the OutputString function in rax 
-mov rdx,qword [efiSystemTable]
-mov rcx,[rdx+EFI_SYSTEM_TABLE.ConOut]
-mov rax,[rcx+SIMPLE_TEXT_OUTPUT_INTERFACE.OutputString]
-xchg rdx,rsi
-;Setup shadow space for GPRs
-sub rsp,32
-call rax
-add rsp,32
-pop rsi
-pop rax
-pop rcx
-pop rdx
 ret
 
 section '.data' readable writable
